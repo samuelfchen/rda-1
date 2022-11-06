5c859be4498ce137acdd80d41c244ac6da619133
Sierra <74543807+sierrathedog@users.noreply.github.com>
2022-11-06 19:00:42 +1100

    initial commit

---
+++ src/CSharp/IRdaSerializable .cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) 2020 Michael Chen
+// Licensed under the MIT License -
+// https://github.com/sierrathedog/rda/blob/main/LICENSE
+
+namespace UniversalDataTransport
+{
+    /*
+     * RDA-Serializable object implements this interface to ensure 
+     * 1) its properties can be stored into an resulting RDA object,  
+     * 2) used a provided RDA to restore this object's properties' values.  
+     * 
+     */
+
+    public interface IRdaSerializable
+    {
+        /// <summary>
+        /// Stores properties into the RDA.
+        /// </summary>
+        /// <returns>An Rda instance that carries the properties of this object.</returns>
+        Rda ToRda();
+
+        /// <summary>
+        /// Populate properties with the values from the RDA
+        /// </summary>
+        /// <param name="rda">An Rda instance that carries the properties of an object to be restored.</param>
+        IRdaSerializable FromRda(Rda rda);
+    }
+}
+
